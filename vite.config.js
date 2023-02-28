818252a768f87c49d9fdb49ff7fc75b4af7a9f39
Frank van de Voorde <daemonaz@kymp.net>
2023-02-28 14:20:24 +0200

    Add tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+const host = 'news-site.test';
+
 export default defineConfig({
+    server: { 
+        host, 
+        hmr: { host } 
+    }, 
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
