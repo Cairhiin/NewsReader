3c1ba89319053c346b29e0ad749cec3e196a3a43
Frank van de Voorde <daemonaz@kymp.net>
2023-03-01 13:39:49 +0200

    Add more styling to articles

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -2,19 +2,22 @@
 @section('content')
 <div class="container mx-auto px-4 py-4 max-w-5xl">
     <h1 class="font-sans text-5xl uppercase font-bold text-slate-500 py-5">Latest News</h1>
-    <div class="grid grid-cols-2 gap-x-5 gap-y-10">
+    <div class="flex flex-wrap gap-10 justify-content">
         @if (count($articles) == 0)
             <p>No articles found!</p>
         @endif
         @foreach ($articles as $article) 
-            <div>
-                <h2 class="font-serif text-slate-100 text-xl font-bold">
-                    <a href="/articles/{{$article['id']}}">{{$article['title']}}</a>
-                </h2>
-                <p class="uppercase text-lime-300 font-bold">{{$article['author']}}</p>
-                <div class="flex py-4">
-                    <img src="default.jpg" />
-                    <p class="px-4">{{explode(".", $article['content'])[0] . "."}}</p>
+            <div class="basis-[47%] shrink flex-initial bg-stone-900/30 rounded-md shadow">
+                <div class="h-64 bg-center" style="background-image: url({{asset('images/default.jpg')}})">
+                    <div class="p-2 text-sky-500">
+                        {{$article['tags']}}
+                    </div>
+                </div>
+                <div class="p-4">
+                    <h2 class="font-serif text-slate-100 text-xl font-bold">
+                        <a href="/articles/{{$article['id']}}">{{$article['title']}}</a>
+                    </h2>
+                    <p class="text-sky-500 font-bold pt-2">{{$article['author']}}</p>
                 </div>
             </div>
         @endforeach
