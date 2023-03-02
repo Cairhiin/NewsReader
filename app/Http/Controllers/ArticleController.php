12100ecee4185053a0e96f19ff0831db636fd63d
Frank van de Voorde <daemonaz@kymp.net>
2023-03-02 15:02:05 +0200

    Add tag filtering

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,7 +9,7 @@ class ArticleController extends Controller
 {
     public function index() {
         return view('articles.index', [
-            'articles' => Article::all()
+            'articles' => Article::latest()->filter(request(['tag']))->get()
         ]);
     }
 
--- resources/views/components/article-tags.blade.php
+++ resources/views/components/article-tags.blade.php
@@ -6,7 +6,9 @@
 <ul class="text-gray-100 text-sm">
     @foreach ($splitTags as $tag)
     <li class="inline-block bg-stone-900 mr-2 py-1 px-3 rounded-3xl align-middle text-center">
-        {{$tag}}
+        <a href="?tag={{trim($tag)}}">
+            {{trim($tag)}}
+        </a>
     </li>
     @endforeach
 </ul>
