556f94776c6ffaa0fb6fabfbd3ae5ca7fc7451c2
Frank van de Voorde <daemonaz@kymp.net>
2023-03-06 19:07:37 +0200

    Add route protection

--- routes/web.php
+++ routes/web.php
@@ -8,25 +8,33 @@ use App\Http\Controllers\ArticleController;
 // Article Routes
 Route::get('/', [ArticleController::class, 'index']);
 
-Route::get('/articles/create', [ArticleController::class, 'create']);
+Route::get('/articles/create', 
+    [ArticleController::class, 'create'])->middleware('auth');
 
-Route::post('/articles', [ArticleController::class, 'store']);
+Route::post('/articles', 
+    [ArticleController::class, 'store'])->middleware('auth');;
 
-Route::get('/articles/{article}/edit', [ArticleController::class, 'edit']);
+Route::get('/articles/{article}/edit', 
+    [ArticleController::class, 'edit'])->middleware('auth');
 
-Route::put('/articles/{article}', [ArticleController::class, 'update']);
+Route::put('/articles/{article}', 
+    [ArticleController::class, 'update'])->middleware('auth');;
 
-Route::delete('/articles/{article}', [ArticleController::class, 'destroy']);
+Route::delete('/articles/{article}', 
+    [ArticleController::class, 'destroy'])->middleware('auth');;
 
 Route::get('/articles/{article}', [ArticleController::class, 'show']);
 
 // User Routes
-Route::get('/register', [UserController::class, 'create']);
+Route::get('/register', 
+    [UserController::class, 'create'])->middleware('guest');
 
 Route::post('/users', [UserController::class, 'store']);
 
-Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/logout', 
+    [UserController::class, 'logout'])->middleware('auth');
 
-Route::get('/login', [UserController::class, 'login']);
+Route::get('/login', 
+    [UserController::class, 'login'])->name('login')->middleware('guest');
 
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
