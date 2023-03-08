2092e68bba84986304876c5eb9b0c2568745603d
Frank van de Voorde <daemonaz@kymp.net>
2023-03-08 22:08:28 +0200

    Add more styling - change the feature component into a grid

--- resources/views/components/article-feature.blade.php
+++ resources/views/components/article-feature.blade.php
@@ -1,13 +1,13 @@
 @props(['articles'])
-<section class="flex justify-center gap-1">
-    <figure class="w-1/2">
+<section class="grid grid-cols-2 grid-rows-2 gap-1 place-content-start mx-32 my-8">
+    <figure>
         <img class="object-none h-full" src="{{$articles[0]->image ? asset('storage/' . $articles[0]->image) : asset('images/default.jpg')}}" />
     </figure>
-    <div class="w-1/2">
-        <figure class="mb-1">
+    <div class="grid grid-cols-2 grid-rows-2 gap-1">
+        <figure class="col-span-2">
             <img class="object-none h-48 w-full" src="{{$articles[1]->image ? asset('storage/' . $articles[1]->image) : asset('images/default.jpg')}}" />
         </figure>
-        <div class="flex gap-1">
+        <div class="flex col-span-2 gap-1">
             <figure class="w-1/2">
                 <img class="object-none h-48 w-full" src="{{$articles[1]->image ? asset('storage/' . $articles[1]->image) : asset('images/default.jpg')}}" />
             </figure>
