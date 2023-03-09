8975c4d5ff6489b6ea7201aeba25761be3154618
Frank van de Voorde <daemonaz@kymp.net>
2023-03-09 10:19:33 +0200

    Fix grid layout

--- resources/views/components/article-feature.blade.php
+++ resources/views/components/article-feature.blade.php
@@ -1,19 +1,21 @@
 @props(['articles'])
-<section class="grid grid-cols-2 grid-rows-2 gap-1 place-content-start mx-32 my-8">
-    <figure>
+<section class="grid grid-cols-4 grid-rows-2 gap-1 place-content-start mx-32 my-8">
+    <figure class="col-span-2 row-span-2">
         <img class="object-none h-full" src="{{$articles[0]->image ? asset('storage/' . $articles[0]->image) : asset('images/default.jpg')}}" />
     </figure>
-    <div class="grid grid-cols-2 grid-rows-2 gap-1">
+    <div class="col-span-2 gap-1">
         <figure class="col-span-2">
             <img class="object-none h-48 w-full" src="{{$articles[1]->image ? asset('storage/' . $articles[1]->image) : asset('images/default.jpg')}}" />
         </figure>
-        <div class="flex col-span-2 gap-1">
-            <figure class="w-1/2">
-                <img class="object-none h-48 w-full" src="{{$articles[1]->image ? asset('storage/' . $articles[1]->image) : asset('images/default.jpg')}}" />
-            </figure>
-            <figure class="w-1/2">
-                <img class="object-none h-48 w-full" src="{{$articles[2]->image ? asset('storage/' . $articles[2]->image) : asset('images/default.jpg')}}" />
-            </figure>
-        </div>
+    </div>
+    <div>
+        <figure>
+            <img class="object-none h-48 w-full" src="{{$articles[1]->image ? asset('storage/' . $articles[1]->image) : asset('images/default.jpg')}}" />
+        </figure>
+    </div>
+    <div>
+        <figure>
+            <img class="object-none h-48 w-full" src="{{$articles[2]->image ? asset('storage/' . $articles[2]->image) : asset('images/default.jpg')}}" />
+        </figure>
     </div>
 </section>
