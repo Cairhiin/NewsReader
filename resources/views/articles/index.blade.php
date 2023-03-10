9990c83bfa7a0dbb6c1fe223a92fba28913efd97
Frank van de Voorde <daemonaz@kymp.net>
2023-03-10 10:57:18 +0200

    Add styling to main page and article card

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,17 +1,18 @@
 <x-layout>   
     <div>
-        <div class="col-span-3">            
+        <div class="col-span-3 mx-32">            
             @if (count($articles) > 3) 
                 <x-article-feature :articles=$articles></x-article-feature>
             @elseif (count($articles) == 0)
                 <p>No articles found!</p>
-            @else
+            @endif          
+            <section class="grid grid-cols-4 gap-8">          
             @foreach ($articles as $article) 
-                @if ($loop->index >= 4)
-                
+                @if ($loop->index >= 4 && $loop->index < 8)              
+                    <x-article-card :article=$article></x-article-card>
                 @endif
             @endforeach
-            @endif
+            </section>
             <div class="mt-6 p-4">
                 {{$articles->links()}}
             </div>
