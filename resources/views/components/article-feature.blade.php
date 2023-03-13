22e36c4eee35e7f310279eef6257ff6fb82354b1
Frank van de Voorde <daemonaz@kymp.net>
2023-03-13 15:59:25 +0200

    Fix misaligned cell by changing the flex gap to a border

--- resources/views/components/article-feature.blade.php
+++ resources/views/components/article-feature.blade.php
@@ -1,15 +1,15 @@
 @props(['articles', 'categories'])
-<section class="grid grid-cols-4 grid-rows-2 gap-1 place-content-start my-4">
+<section class="grid grid-cols-4 grid-rows-2 gap-x-1 place-content-start my-4">
     <div class="col-span-2 row-span-2 relative">
         <figure class="h-full">
-            <img class="object-none h-full" src="{{$articles[0]->image ? asset('storage/' . $articles[0]->image) : asset('images/default.jpg')}}" />
+            <img class="object-none h-96 w-full" src="{{$articles[0]->image ? asset('storage/' . $articles[0]->image) : asset('images/default.jpg')}}" />
             <figcaption class="absolute bottom-16 px-4">
                 <a href="">{{$articles[0]->category->name}}</a>
                 <h2 class="text-white font-bold text-2xl drop-shadow truncate">{{$articles[0]->title}}</h2>
             </figcaption>
         </figure>
     </div>
-    <div class="col-span-2 gap-1 relative">
+    <div class="col-span-2 relative">
         <figure class="col-span-2">
             <img class="object-none h-48 w-full" src="{{$articles[1]->image ? asset('storage/' . $articles[1]->image) : asset('images/default.jpg')}}" />
             <figcaption class="absolute bottom-8 px-4">
@@ -20,7 +20,7 @@
     </div>
     <div>
         <figure class="relative">
-            <img class="object-none h-48 w-full" src="{{$articles[1]->image ? asset('storage/' . $articles[1]->image) : asset('images/default.jpg')}}" />
+            <img class="box-border border-t-4 object-none h-48 w-full" src="{{$articles[1]->image ? asset('storage/' . $articles[1]->image) : asset('images/default.jpg')}}" />
             <figcaption class="absolute top-28 px-2 ">
                 <a href="">{{$articles[2]->category->name}}</a>
                 <h2 class="text-white text-base font-bold drop-shadow leading-4">{{$articles[2]->title}}</h2>
@@ -29,7 +29,7 @@
     </div>
     <div>
         <figure class="relative">
-            <img class="object-none h-48 w-full" src="{{$articles[2]->image ? asset('storage/' . $articles[2]->image) : asset('images/default.jpg')}}" />
+            <img class="box-border border-t-4 object-none h-48 w-full" src="{{$articles[2]->image ? asset('storage/' . $articles[2]->image) : asset('images/default.jpg')}}" />
             <figcaption class="absolute top-28 px-2">
                 <a href="">{{$articles[3]->category->name}}</a>
                 <h2 class="text-white text-base font-bold drop-shadow leading-4">{{$articles[3]->title}}</h2>
