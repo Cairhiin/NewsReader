beff0f72dade2c846161822f47c830a155f7fe31
Frank van de Voorde <daemonaz@kymp.net>
2023-03-14 13:28:38 +0200

    Add styling to show Article

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,101 +1,103 @@
 <x-layout>
-    <header class="text-center">
-        <h2 class="text-2xl font-bold uppercase mb-1">
-            Add a new article
-        </h2>
-    </header>
+    <div class="max-w-screen-sm mx-auto px-8 py-4 my-4 bg-stone-200 rounded-md">
+        <header class="text-center">
+            <h2 class="text-2xl font-bold uppercase mb-1">
+                Add a new article
+            </h2>
+        </header>
 
-    <form method="POST" action="/articles" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-6">
-            <label
-                for="title"
-                class="inline-block text-lg mb-2"
-                >Title</label
-            >
-            <input
-                type="text"
-                class="border border-gray-200 rounded p-2 w-full"
-                name="title"
-                placeholder="Title"
-                value="{{old('title')}}"
-            />
-            @error('title')
-                <p class="text-red-500 mt-1">{{$message}}</p>
-            @enderror
-        </div>
+        <form method="POST" action="/articles" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-6">
+                <label
+                    for="title"
+                    class="inline-block text-lg mb-2"
+                    >Title</label
+                >
+                <input
+                    type="text"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="title"
+                    placeholder="Title"
+                    value="{{old('title')}}"
+                />
+                @error('title')
+                    <p class="text-red-500 mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-        <div class="mb-6">
-            <label class="block mb-2" for="category" class="inline-block text-lg mb-2">
-                Category
-            </label>
-            <select name="category_id" id="category">
-                @foreach($categories as $category)
-                    <option value="{{$category->id}}">{{$category->name}}</option>
-                @endforeach
-            </select>
-            @error('category_id')
-                <p class="text-red-500 mt-1">{{$message}}</p>
-            @enderror
-        </div>
+            <div class="mb-6">
+                <label class="block mb-2" for="category" class="inline-block text-lg mb-2">
+                    Category
+                </label>
+                <select name="category_id" id="category">
+                    @foreach($categories as $category)
+                        <option value="{{$category->id}}">{{$category->name}}</option>
+                    @endforeach
+                </select>
+                @error('category_id')
+                    <p class="text-red-500 mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-        <div class="mb-6">
-            <label for="tags" class="inline-block text-lg mb-2">
-                Tags (Comma Separated)
-            </label>
-            <input
-                type="text"
-                class="border border-gray-200 rounded p-2 w-full"
-                name="tags"
-                placeholder="Example: sport, politics, etc"
-                value="{{old('tags')}}"
-            />
-            @error('tags')
-                <p class="text-red-500 mt-1">{{$message}}</p>
-            @enderror
-        </div>
+            <div class="mb-6">
+                <label for="tags" class="inline-block text-lg mb-2">
+                    Tags (Comma Separated)
+                </label>
+                <input
+                    type="text"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="tags"
+                    placeholder="Example: sport, politics, etc"
+                    value="{{old('tags')}}"
+                />
+                @error('tags')
+                    <p class="text-red-500 mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-        <div class="mb-6">
-            <label for="image" class="inline-block text-lg mb-2">
-                Image
-            </label>
-            <input
-                type="file"
-                class="border border-gray-200 rounded p-2 w-full"
-                name="image"
-                value="{{old('image')}}"
-            />
-            @error('image')
-                <p class="text-red-500 mt-1">{{$message}}</p>
-            @enderror
-        </div>
+            @if ()
+            <div class="mb-6">
+                <label for="image" class="inline-block text-lg mb-2">
+                    Image
+                </label>
+                <input
+                    type="file"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="image"
+                    value="{{old('image')}}"
+                />
+                @error('image')
+                    <p class="text-red-500 mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-        <div class="mb-6">
-            <label
-                for="content"
-                class="inline-block text-lg mb-2"
-            >
-                Content
-            </label>
-            <textarea
-                class="border border-gray-200 rounded p-2 w-full"
-                name="content"
-                rows="10"
-                placeholder="Article main content"              
-            >{{old('content')}}</textarea>
-            @error('content')
-                <p class="text-red-500 mt-1">{{$message}}</p>
-            @enderror
-        </div>
+            <div class="mb-6">
+                <label
+                    for="content"
+                    class="inline-block text-lg mb-2"
+                >
+                    Content
+                </label>
+                <textarea
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="content"
+                    rows="10"
+                    placeholder="Article main content"              
+                >{{old('content')}}</textarea>
+                @error('content')
+                    <p class="text-red-500 mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-        <div class="mb-6">
-            <button
-                class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
-            >
-                Add article
-            </button>
+            <div class="mb-6">
+                <button
+                    class="bg-sky-600 hover:bg-sky-800 text-white rounded py-2 px-4 transition-colors duration-500 easeout"
+                >
+                    Add article
+                </button>
 
-            <a href="/" class="text-black ml-4"> Back </a>
-        </div>
-    </form>
+                <a href="/" class="text-black ml-4"> Back </a>
+            </div>
+        </form>
 </x-layout>
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,18 +1,32 @@
 <x-layout>
-    <div class="mx-auto px-4 py-4 max-w-5xl">
+    <div class="mx-auto my-4 py-4 px-8 max-w-screen-md bg-white rounded-md">
         <div>
-            <p class="uppercase text-lime-300 font-bold">{{$article->author->name}}</p>
-            <h1 class="font-serif text-slate-100 text-3xl font-bold">{{$article['title']}}</h1>
-            <figure class="relative">
-                <img 
-                    class="w-full aspect-[11/6] object-none" 
-                    src="{{$article->image ? asset('storage/' . $article->image) : asset('images/default.jpg')}}" 
-                />
-                <figcaption class="absolute bottom-4 left-4">
-                    {{-- TODO: Implement a caption --}}
-                </figcaption>
-            </figure>
-            <p class="py-4">{{$article['content']}}</p>
+            <h1 class="font-serif text-stone-700 text-3xl font-bold">{{$article['title']}}</h1>
+            <p class="text-stone-400 text-sm"> 
+                {{date('d.m.Y', strtotime($article->updated_at))}}    
+            </p> 
+            @if ($article->category->name != 'opinion')
+                <figure class="relative">
+                    <img 
+                        class="w-full aspect-[11/6] object-none" 
+                        src="{{$article->image ? asset('storage/' . $article->image) : asset('images/default.jpg')}}" 
+                    />
+                    <figcaption class="absolute bottom-4 left-4">
+                        {{-- TODO: Implement a caption --}}
+                    </figcaption>
+                </figure>
+            @endif
+            <p class="py-2">{{$article['content']}}</p>
+        </div>
+        <div class="flex items-center my-5">
+            @if ($article->author->image)
+                <img src="{{asset('storage/' . $article->author->image)}}" class="w-24 h-24 rounded-full mr-2">
+            @endif
+            <div>
+                <p class="pt-1 text-xl font-bold text-stone-700">
+                    {{$article->author->name}}
+                </p> 
+            </div>
         </div>
         <div>
             <a href="/articles/{{$article->id}}/edit">
