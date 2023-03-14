5daa5dd5289537cfe4c7552727b88144ca9c5dc8
Frank van de Voorde <daemonaz@kymp.net>
2023-03-14 11:28:53 +0200

    Add styling to login and register forms

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,61 +1,63 @@
 <x-layout>
-    <header class="text-center">
-        <h2 class="text-2xl font-bold uppercase mb-1">
-            Login
-        </h2>
-        <p class="mb-4">Log into your account</p>
-    </header>
-
-    <form method="POST" action="/users/authenticate">
-        @csrf
-        <div class="mb-6">
-            <label for="email" class="inline-block text-lg mb-2"
-                >Email</label
-            >
-            <input
-                type="email"
-                class="border border-gray-200 rounded p-2 w-full"
-                name="email"
-                value="{{old('email')}}"
-            />
-            @error('email')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-6">
-            <label
-                for="password"
-                class="inline-block text-lg mb-2"
-            >
-                Password
-            </label>
-            <input
-                type="password"
-                class="border border-gray-200 rounded p-2 w-full"
-                name="password"
-            />
-            @error('password')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
-        </div>
-
-        <div class="mb-6">
-            <button
-                type="submit"
-                class="bg-sky-600 text-white rounded py-2 px-4 hover:bg-black"
-            >
+    <div class="max-w-screen-sm mx-auto px-8 py-4 my-4 bg-stone-200 rounded-md">
+        <header class="text-center">
+            <h2 class="text-2xl font-bold uppercase mb-1">
                 Login
-            </button>
-        </div>
+            </h2>
+            <p class="mb-4">Log into your account</p>
+        </header>
 
-        <div class="mt-8">
-            <p>
-                Don't have an account?
-                <a href="/register" class="bg-stone-700">
-                    Register
-                </a>
-            </p>
-        </div>
-    </form>
+        <form method="POST" action="/users/authenticate">
+            @csrf
+            <div class="mb-6">
+                <label for="email" class="inline-block text-lg mb-2"
+                    >Email</label
+                >
+                <input
+                    type="email"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="email"
+                    value="{{old('email')}}"
+                />
+                @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <label
+                    for="password"
+                    class="inline-block text-lg mb-2"
+                >
+                    Password
+                </label>
+                <input
+                    type="password"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="password"
+                />
+                @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <button
+                    type="submit"
+                    class="bg-sky-600 hover:bg-sky-800 text-white rounded py-2 px-4 transition-colors duration-500 easeout"
+                >
+                    Login
+                </button>
+            </div>
+
+            <div class="mt-8">
+                <p>
+                    Don't have an account?
+                    <a href="/register">
+                        Register
+                    </a>
+                </p>
+            </div>
+        </form>
+    </div>
 </x-layout>
