2f316dae4bdddba5f1b61192453683e66c7228f2
Frank van de Voorde <daemonaz@kymp.net>
2023-03-15 11:18:53 +0200

    Finish sidebar component styling

--- resources/views/components/opinion-sidebar.blade.php
+++ resources/views/components/opinion-sidebar.blade.php
@@ -1,14 +1,16 @@
 @props(['opinions'])
 <div {{ $attributes->merge(['class' => 'my-3']) }}>
     @foreach ($opinions as $opinion)
-    <figure class="flex p-3 items-center gap-4 mb-1 overflow-hidden relative">
-        <div>
-            <figcaption class="grow">
-                <h3 class="font-bold text-sm xl:text-lg">{{$opinion->title}}</h3>
-                <p class="text-sm text-stone-400">{{$opinion->author->name}}</p>
-            </figcaption>
-        </div>
-        <img class="w-32 h-32 rounded-full absolute -right-8 border-16 border-slate-300 ring-16 ring-zinc-100" src="{{$opinion->author->image ? asset('storage/' . $opinion->author->image) : asset('images/default.jpg')}}" />
-    </figure>    
+        @if ($loop->index < 5)
+        <figure class="flex p-3 pr-32 h-20 border-b-2 border-zinc-100 bg-white items-center gap-4 overflow-hidden relative">
+            <div>
+                <figcaption class="grow">
+                    <h3 class="font-bold text-sm text-stone-700 leading-5 xl:text-base xl:leading-5">{{$opinion->title}}</h3>
+                    <p class="text-sm text-stone-400 leading-5">{{$opinion->author->name}}</p>
+                </figcaption>
+            </div>
+            <img class="w-32 h-32 rounded-full absolute -right-8 border-16 border-slate-300 ring-16 ring-zinc-100" src="{{$opinion->author->image ? asset('storage/' . $opinion->author->image) : asset('images/default.jpg')}}" />
+        </figure>
+        @endif    
     @endforeach
 </div>
