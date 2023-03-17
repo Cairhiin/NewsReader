7bcb05bfec5a3cfd037288f7f14533e82ef57b1c
Frank van de Voorde <daemonaz@kymp.net>
2023-03-17 13:17:25 +0200

    Fix simple article component styling

--- resources/views/components/article/simple-card.blade.php
+++ resources/views/components/article/simple-card.blade.php
@@ -1,14 +1,14 @@
 @props(['article'])
 <article {{ $attributes->merge(['class' => 'my-3']) }}>
     <a href="articles/{{$article->id}}" class="flex gap-4">
-        <figure>
-            <img class="w-32 h-full" src="{{$article->image ? asset('storage/' . $article->image) : asset('images/default.jpg')}}" />
+        <figure class="w-32">
+            <img class="h-full object-fit" src="{{$article->image ? asset('storage/' . $article->image) : asset('images/default.jpg')}}" />
         </figure>
         <div class="grow pr-4">
             <h3 class="font-bold text-lg text-stone-700">
                 {{$article->title}}
             </h3>
-            <p>
+            <p class="leading-5 truncate h-12">
                 {{explode('.', $article->content)[0] . '.'}}           
             </p> 
             <p class="pt-1 text-right">
