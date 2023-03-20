d29d1bda1707c59ce5c24fc05469870225d966ef
Frank van de Voorde <daemonaz@kymp.net>
2023-03-20 15:49:49 +0200

    Add a height to the button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['class' => 'w-32 text-left text-white rounded py-2 px-4 transition-colors duration-500 easeout'])}}>
+<button {{ $attributes->merge(['class' => 'h-10 w-32 text-left text-white rounded py-2 px-4 transition-colors duration-500 easeout'])}}>
     {{$slot}}
 </button>
