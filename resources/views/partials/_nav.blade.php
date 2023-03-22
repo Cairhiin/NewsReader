b205c9d9fa20ad025606c7ba393ed895378583dd
Frank van de Voorde <daemonaz@kymp.net>
2023-03-22 13:26:16 +0200

    Add fullscreen height to the menu

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -9,26 +9,28 @@
             </a>
         </div>
         <div>
-        @auth
-            <div class="inline mx-2 font-bold invisible md:visible">Welcome, {{Auth()->user()->name}}</div>
-            <div class="md:inline ml-2 mr-4">
-                <a class="mx-2" href="/articles/manage"><i class="fa-solid fa-gear"></i> Manage articles</a>
-                <form class="md:inline ml-2" method="POST" action="/logout">
-                    @csrf
-                    <button type="submit">
-                        <i class="fa-solid fa-door-closed"></i> Logout
-                    </button>
-                </form>
+            <div id="menu" 
+                class="absolute w-full h-screen -left-full aria-checked:left-0 md:relative md:left-0 transition-all easeout duration-500
+                    md:block md:inline md:ml-2 md:mr-4 bg-slate-800 md:bg-stone-900"
+            >
+            @auth
+                <div class="inline mx-2 font-bold invisible md:visible">Welcome, {{Auth()->user()->name}}</div>
+                <div class="md:inline ml-2 mr-4">
+                    <a class="mx-2" href="/articles/manage"><i class="fa-solid fa-gear"></i> Manage articles</a>
+                    <form class="md:inline ml-2" method="POST" action="/logout">
+                        @csrf
+                        <button type="submit">
+                            <i class="fa-solid fa-door-closed"></i> Logout
+                        </button>
+                    </form>
+                </div>
+            @else
+            <div class="divide-y divide-slate-900">
+                <a class="block md:mx-2 md:inline p-3" href="/register"><i class="fa-solid fa-user-plus pr-2"></i> Register</a>
+                <a class="block md:inline p-3" href="/login"><i class="fa-solid fa-arrow-right-to-bracket pr-2"></i> Login</a>
             </div>
-        @else
-        <div id="menu" 
-            class="absolute w-full -left-full aria-checked:left-0 md:relative md:left-0 transition-all easeout duration-500
-                md:block md:inline md:ml-2 md:mr-4 bg-slate-800 divide-y divide-slate-900 md:bg-stone-900"
-        >
-            <a class="block md:mx-2 md:inline p-3" href="/register"><i class="fa-solid fa-user-plus pr-2"></i> Register</a>
-            <a class="block md:inline p-3" href="/login"><i class="fa-solid fa-arrow-right-to-bracket pr-2"></i> Login</a>
-        </div>
-        @endauth
+            @endauth
+            </div>
         </div>
     </div>
     @if ($categories)
