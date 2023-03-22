4d7486cb07e59eea5105fac89fa6bf89832c58d5
Frank van de Voorde <daemonaz@kymp.net>
2023-03-22 13:01:27 +0200

    Add responsive hamburger menu

---
+++ public/scripts/nav.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menuToggle');
+let menuIsOpen = document.getElementById('menu').getAttribute('aria-checked');
+menuToggle.addEventListener('click', () => {
+    menuIsOpen = !menuIsOpen;
+    document.getElementById('menu').setAttribute('aria-checked', menuIsOpen); 
+});
--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,6 +1,7 @@
 <nav class="sticky top-0 z-10">
-    <div class="flex justify-between items-center bg-stone-900 text-gray-100 p-3">
-        <div>
+    <div class="flex flex-col md:flex-row md:justify-between md:items-center bg-stone-900 text-gray-100 md:p-3">
+        <div class="flex items-center p-3 md:p-0">
+            <i id="menuToggle" class="fa-solid fa-bars text-2xl visible md:-ml-6 md:invisible"></i>
             <a href="/">
                 <h1 class="text-3xl font-bold uppercase mx-4">
                     News Reader
@@ -9,10 +10,10 @@
         </div>
         <div>
         @auth
-            <div class="inline mx-2 font-bold">Welcome, {{Auth()->user()->name}}</div>
-            <div class="inline ml-2 mr-4">
+            <div class="inline mx-2 font-bold invisible md:visible">Welcome, {{Auth()->user()->name}}</div>
+            <div class="md:inline ml-2 mr-4">
                 <a class="mx-2" href="/articles/manage"><i class="fa-solid fa-gear"></i> Manage articles</a>
-                <form class="inline ml-2" method="POST" action="/logout">
+                <form class="md:inline ml-2" method="POST" action="/logout">
                     @csrf
                     <button type="submit">
                         <i class="fa-solid fa-door-closed"></i> Logout
@@ -20,9 +21,12 @@
                 </form>
             </div>
         @else
-        <div class="inline ml-2 mr-4">
-            <a class="mx-2" href="/register"><i class="fa-solid fa-user-plus"></i> Register</a>
-            <a href="/login"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+        <div id="menu" 
+            class="absolute w-full -left-full aria-checked:left-0 md:relative md:left-0 transition-all easeout duration-500
+                md:block md:inline md:ml-2 md:mr-4 bg-slate-800 divide-y divide-slate-900 md:bg-stone-900"
+        >
+            <a class="block md:mx-2 md:inline p-3" href="/register"><i class="fa-solid fa-user-plus pr-2"></i> Register</a>
+            <a class="block md:inline p-3" href="/login"><i class="fa-solid fa-arrow-right-to-bracket pr-2"></i> Login</a>
         </div>
         @endauth
         </div>
@@ -39,3 +43,4 @@
     </div>
     @endif
 </nav>
+<script type="text/javascript" src="/scripts/nav.js"></script>
