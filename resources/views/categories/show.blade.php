85fd8c168450a4d1232ffae04fe6c91f24f3af41
Frank van de Voorde <daemonaz@kymp.net>
2023-03-24 13:10:59 +0200

    Fix the links to the news articles on the category specific view

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -5,53 +5,59 @@
         @unless ($category->name === 'opinion')
         <section class="flex justify-center gap-8">
             @foreach ($articles as $article)
-                @if ($loop->index < 2)
-                    <figure class="relative">
-                        <img 
-                            class="object-fit" 
-                            src="{{$article->image ? asset('storage/' . $article->image) : asset('images/default.jpg')}}" 
-                        />
-                        <figcaption>
-                            <h2 class="text-shadow absolute bottom-0 py-4 px-4 text-white font-bold text-2xl drop-shadow leading-7">
-                                {{$article->title}}
-                            </h2>
-                        </figcaption>              
-                    </figure> 
+                <a href="/articles/{{$article->id}}">
+                    @if ($loop->index < 2)
+                        <figure class="relative">
+                            <img 
+                                class="object-fit" 
+                                src="{{$article->image ? asset('storage/' . $article->image) : asset('images/default.jpg')}}" 
+                            />
+                            <figcaption>
+                                <h2 class="text-shadow absolute bottom-0 py-4 px-4 text-white font-bold text-2xl drop-shadow leading-7">
+                                    {{$article->title}}
+                                </h2>
+                            </figcaption>                                   
+                        </figure> 
+                    </a>
                     @if ($loop->index == 1)
                         </section>
                         <div class="flex flex-wrap lg:flex-nowrap">
                             <section class="grid grid-cols-3 gap-8 my-8 basis-2/3 grow">
                     @endif
                 @else
-                    <figure>
-                        <img 
-                            class="object-fit rounded" 
-                            src="{{$article->image ? asset('storage/' . $article->image) : asset('images/default.jpg')}}" 
-                        />
-                        <figcaption>
-                            <h3 class="py-2 text-stone-700 dark:text-slate-400 text-lg font-bold leading-5">
-                                {{$article->title}}
-                            </h3>
-                            <p class="dark:text-slate-200">
-                                {{ explode('.', $article->content)[0] . '.'}}
-                            </p>
-                        </figcaption>               
-                    </figure>
+                    <a href="/articles/{{$article->id}}">
+                        <figure>
+                            <img 
+                                class="object-fit rounded" 
+                                src="{{$article->image ? asset('storage/' . $article->image) : asset('images/default.jpg')}}" 
+                            />
+                            <figcaption>
+                                <h3 class="py-2 text-stone-700 dark:text-slate-400 text-lg font-bold leading-5">
+                                    {{$article->title}}
+                                </h3>
+                                <p class="dark:text-slate-200">
+                                    {{ explode('.', $article->content)[0] . '.'}}
+                                </p>
+                            </figcaption>                                       
+                        </figure>
+                    </a>
                 @endif
             @endforeach
                 </section>
             @else
                 <section class="rounded-md shadow-md bg-white dark:bg-gray-800 divide-y divide-slate-100 dark:divide-slate-500 px-4">
                 @foreach ($articles as $article)
-                    <div class="flex gap-4 p-2 items-center">
-                        <figure class="basis-24 flex-none">
-                            <img class="w-24 rounded-full" src="{{$article->author->image ? asset('storage/' . $article->author->image) : asset('images/default.jpg')}}" />
-                        </figure>
-                        <div class="flex-initial">
-                            <h3 class="font-bold lg:text-base xl:text-lg dark:text-slate-400">{{$article->title}}</h3>
-                            <p class="uppercase text-sm dark:text-slate-200">{{$article->author->name}}</p>
+                    <a href="/articles/{{$article->id}}">
+                        <div class="flex gap-4 p-2 items-center">
+                                <figure class="basis-24 flex-none">
+                                    <img class="w-24 rounded-full" src="{{$article->author->image ? asset('storage/' . $article->author->image) : asset('images/default.jpg')}}" />
+                                </figure>
+                                <div class="flex-initial">
+                                    <h3 class="font-bold lg:text-base xl:text-lg dark:text-slate-400">{{$article->title}}</h3>
+                                    <p class="uppercase text-sm dark:text-slate-200">{{$article->author->name}}</p>
+                                </div>
                         </div>
-                    </div>
+                    </a>
                 @endforeach
                 </section>
             </div>
@@ -62,8 +68,10 @@
                 <div class="flex flex-col divide-y divide-slate-100 dark:divide-slate-500 bg-white dark:bg-gray-800 rounded mr-0 p-4">
                 @foreach ($populairArticles as $article)
                     <div class="py-4 last:pb-0">
-                        <p class="text-sm text-stone-400 dark:text-slate-400">{{date('d.m.Y', strtotime($article->updated_at))}}</p>
-                        <h4 class="dark:text-slate-200">{{$article->title}}</h4>
+                        <a href="/articles/{{$article->id}}">
+                            <p class="text-sm text-stone-400 dark:text-slate-400">{{date('d.m.Y', strtotime($article->updated_at))}}</p>
+                            <h4 class="dark:text-slate-200">{{$article->title}}</h4>
+                        </a>
                     </div>
                 @endforeach
                 </div>
